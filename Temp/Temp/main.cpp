5379b2a6f8f60ec0d6d7745b36cb59e406597468
jmora526 <jmorales4255@gmail.com>
2017-09-18 10:12:01 -0700

    Signed-off-by: jmora526 <jmorales4255@gmail.com>

---
+++ Temp/Temp/main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File: 
+ * Author: Jose Morales
+ * Purpose:
+ * Created on September 18, 2017, 11:59 PM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+   
+    return 0;
+}
